159635d1ab39614e71778184ad31377182c22106
1ubush <je1971sus@gmail.com>
2022-10-04 16:50:19 +0300

    contacts.index

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
 for (let i = 1; i < 5; i++) {
-   document.write(i + "<br>");
+   document.write(`
+   <div id="number" class="table">${i}</div>
+   <div id="names" class="table"></div>
+   <div id="work" class="table"></div>
+   <div id="email" class="table"></div>
+   <div id="numbersOne" class="table"></div>
+   <div id="numbersTwo" class="table"></div>
+   <div id="city" class="table"></div>
+   `);
 }
--- script/slider.js
+++ script/slider.js
@@ -2,17 +2,17 @@ let offset = 0;
 const sliderLine = document.querySelector('.slider_line');
 
 document.querySelector('.next').addEventListener('click', function () {
-   offset = offset + (-247.5);
+   offset = offset + 247.5;
    if (offset > 0) {
       offset = 990
    }
-   sliderLine.style.top = offset + 'px';
+   sliderLine.style.top = -offset + 'px';
 });
 
 document.querySelector('.prev').addEventListener('click', function() {
-   offset = offset + 247.5;
-   if (offset < 990) {
-      offset = 0
+   offset = offset - 247.5;
+   if (offset < 0) {
+      offset = 990
    }
-   sliderLine.style.top = offset + 'px';
-})
+   sliderLine.style.top = -offset + 'px';
+});
