c315e139772335ecb150620167d0297d53d0ad52
1ubush <je1971sus@gmail.com>
2022-10-06 16:05:52 +0300

    contacts

--- script/script.js
+++ script/script.js
@@ -1,11 +1,20 @@
-for (let i = 1; i < 5; i++) {
+baglay = ["Баглай Андрій Олександрович", "Головний інженер",
+         "worldinject@ukr.net", "+38044 2079758", "+38067 5589412"];
+
+two = ["English, Deutsch", "Project Managerin", "wasserprojekt@radoy.com.ua",
+       "+38095 4049033", "+38067 5589410"];
+       
+kolesnichenko = ["Колесниченко Альона Вікторівна", "Проект-менеджер",
+                 "wasserprojekt@radoy.com.ua", "+38095 4049033", "+38067 5589410"];
+
+for (let i = 1; i < 6; i++) {
    document.write(`
-   <div id="number" class="table">${i}</div>
-   <div id="names" class="table"></div>
-   <div id="work" class="table"></div>
-   <div id="email" class="table"></div>
-   <div id="numbersOne" class="table"></div>
-   <div id="numbersTwo" class="table"></div>
-   <div id="city" class="table"></div>
+   <div class="info_block">
+      <div class="content_block">
+         ${baglay[-1 + i] + "<br>"} <br>
+         ${two[-1 + i] + "<br>"} <br>
+         ${kolesnichenko[-1 + i]}
+      </div>
+   </div>
    `);
 }
