3ca44ea75a94065bc144ed7bcf468cac457202e3
1ubush <je1971sus@gmail.com>
2022-10-11 21:45:13 +0300

    slide.fix for js

--- script/slider.js
+++ script/slider.js
@@ -1,18 +1,24 @@
 let offset = 0;
 const sliderLine = document.querySelector('.slider_line');
+let timer;
+autoSlide();
 
-document.querySelector('.next').addEventListener('click', function () {
-   offset = offset + 247.5;
-   if (offset > 0) {
-      offset = 990
+function autoSlide() {
+   setInterval(2000);
+}
+
+document.querySelector('.next').addEventListener('click', function next() {
+   offset = offset + 475.75;
+   if (offset > 1903) {
+      offset = 1903;
    }
-   sliderLine.style.top = -offset + 'px';
+   sliderLine.style.left = -offset + 'px';
 });
 
-document.querySelector('.prev').addEventListener('click', function() {
-   offset = offset - 247.5;
-   if (offset < 0) {
-      offset = 990
+document.querySelector('.prev').addEventListener('click', function next() {
+   offset = offset - 475.75;
+   if (offset < 1903) {
+      offset = 0;
    }
-   sliderLine.style.top = -offset + 'px';
+   sliderLine.style.left = -offset + 'px';
 });
